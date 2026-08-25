a464b490febb1605505d488cbebc7803cef02f4e
Guille Lavado <lavadoguille@gmail.com>
2026-08-25 18:26:04 +0200

    Cambio en la estructura del proyecto

--- src/index.js
+++ src/index.js
@@ -2,24 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import { Typer, MultiTyper } from './Typer';
+import {Typer, MultiTyper} from './Typer';
 import reportWebVitals from './reportWebVitals';
+
+const saludos = [
+  "Hola Mundo.",
+  "Hello Word.",
+];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    {/* </React.StrictMode> */}
-      {/* <App /> */}
-      <MultiTyper 
-        texts={[
-          "Primer texto que se escribe.",
-          "Segundo texto en secuencia.",
-          "Tercer y último texto.",
-        ]}
-      />
-      <Typer text="Hola Mundo" repeat={false} />
-    {/* </React.StrictMode> */}
-  </>
+  <React.StrictMode>
+    {/* <App /> */}
+    <Typer text="Guille Lavado"/>
+    <MultiTyper texts={saludos} />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
