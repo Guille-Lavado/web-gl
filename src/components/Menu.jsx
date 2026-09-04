4d56cfd09bae79685534db63be316073f81be00a
Guille Lavado <lavadoguille@gmail.com>
2026-09-04 12:45:57 +0200

    Cambio en el diseño del menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,18 +4,18 @@ function Menu({theme, toggleTheme}) {
   return (
     <nav className='menu'>
       <span className='logo'>&#62;&#95;</span>
-      <ul>
-        <li><a href="#saludo">Sobre mí</a></li>
-        <li><a href="#repositorios">Proyectos</a></li>
-        <li><a href="#experiencia">Experiencia</a></li>
-        <li><a href="#estudios">Titulación</a></li>
-        <li><a href="#contacto">Contacto</a></li>
-        <li>
-          <button className="btn-theme" onClick={toggleTheme}>
-            {theme === 'dark' ? 'Claro' : 'Oscuro'}
-          </button>
-        </li>
-      </ul>
+      <div>
+        <ul>
+          <li><a href="#saludo">Sobre mí</a></li>
+          <li><a href="#repositorios">Proyectos</a></li>
+          <li><a href="#experiencia">Experiencia</a></li>
+          <li><a href="#estudios">Titulación</a></li>
+          <li><a href="#contacto">Contacto</a></li>
+        </ul>
+        <button className="btn-theme" onClick={toggleTheme}>
+          {theme === 'dark' ? 'Claro' : 'Oscuro'}
+        </button>
+      </div>
     </nav>
   );
 }
